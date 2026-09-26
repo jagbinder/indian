d22740ef1febaebb4d238512c0aa8b0cf8697e44
mycode <jsbomrah@gmail.com>
2026-09-26 10:04:08 +0530

    java changes

---
+++ org.java
@@ -0,0 +1,16 @@
+class  kite
+{
+
+    int a,b;
+    void getData()
+    {
+        System.out.println(a+b);
+    }
+    public static void main(String h[])
+    {
+    kite kk=new kite();
+    kk.a=890;
+    kk.b=200;
+    kk.getData();        
+    }
+}
